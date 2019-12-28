ee2056ceaf4fb9820f45e02df4d1db888d38ad78
aragak1yu1 <601841941@qq.com>
2019-12-28 16:41:06 +0800

    springboot-helloworld

---
+++ 01-springboot-hello/src/main/java/com/yf/App.java
@@ -0,0 +1,19 @@
+package com.yf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Springboot启动类
+ * 
+ * @author yangf
+ *
+ */
+@SpringBootApplication
+public class App {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(App.class, args);
+		
+	}
+}
